f05635533e6bed9a85a823a6a1352de2c24c873e
szroly <sep.roland@gmail.com>
2025-11-18 20:16:30 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Orders,
+  Products,
+  Register,
+  Login,
+  SingleProduct,
+} from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "products/:id",
+        element: <SingleProduct />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "checkout",
+        element: <Checkout />,
+      },
+      {
+        path: "orders",
+        element: <Orders />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <Error />,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
---
+++ src/components/FormInput.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const FormInput = ({ label, name, type, defaultValue }) => {
+  return (
+    <fieldset className="fieldset">
+      <legend className="fieldset-legend">{label}</legend>
+      <input
+        name={name}
+        type={type}
+        className="input"
+        defaultValue={defaultValue}
+      />
+    </fieldset>
+  );
+};
+
+export default FormInput;
