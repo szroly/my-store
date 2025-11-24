320136ad9a7741e41268dc3a9d7fc85004340cf1
szroly <sep.roland@gmail.com>
2025-11-24 21:41:55 +0100

    Navbar + theme

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
+import { Header, Navbar } from "../components";
 
 const HomeLayout = () => {
   return (
     <>
-      <nav>
-        <span className="text-4xl text-primary">Comfy</span>
-      </nav>
-      <Outlet />
+      <Header />
+      <Navbar />
+      <section className="align-element py-20">
+        <Outlet />
+      </section>
     </>
   );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { Form, Link } from "react-router-dom";
+import { FormInput, SubmitBtn } from "../components";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <section className="h-screen grid place-items-center">
+      <Form
+        method="POST"
+        className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
+      >
+        <h4 className="text-center text-3xl font-bold">Register</h4>
+        <FormInput type="text" name="username" label="username" />
+        <FormInput type="email" name="email" label="email" />
+        <FormInput type="password" name="password" label="password" />
+        <div className="mt-4">
+          <SubmitBtn text="register" />
+        </div>
+        <p className="text-center">
+          Alredy a member?
+          <Link
+            to="/login"
+            className="ml-2 link link-hover link-primary capitalize"
+          >
+            login
+          </Link>
+        </p>
+      </Form>
+    </section>
+  );
 };
 
 export default Register;
